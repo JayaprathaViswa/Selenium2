ee4b61d191fdf442c73b6741dde959b573ae2a98
JayaprathaViswa <Pratha.rj@gmail.com>
2022-07-21 16:45:57 +0530

    initial one

---
+++ src/org/test/Employee.java
@@ -0,0 +1,31 @@
+package org.test;
+
+public class Employee {
+private void employeeDetails() {
+System.out.println("EmployeeDetails");
+}
+//differ by datatype
+private void employeeDetails(float age) {
+System.out.println("Employee age:"+" "+age);
+}
+private void employeeDetails(int age) {
+System.out.println("Employee age:"+" "+age);}
+//differ by datatype order
+private void employeeDetails(int year, int date){
+	System.out.println("Employee year: "+  year +"\n"+ "Employee date: " + date);
+	
+}
+private void employeeDetails(String name){
+	System.out.println("Employee Name: "+ name );
+}
+
+public static void main(String[] args) {
+	Employee e= new Employee();
+	e.employeeDetails(4);
+	e.employeeDetails(16);
+	e.employeeDetails(1996, 14);
+			e.employeeDetails("Akshaya");
+}
+
+
+}
